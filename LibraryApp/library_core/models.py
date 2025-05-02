d287eca26a0efb1049519bf09d75d896ac6f62ee
Beyzanur <beyzanurdincer502@gmail.com>
2025-05-02 18:33:43 +0300

    Database records successfully added. User and superadmin created with email and password.

--- LibraryApp/library_core/models.py
+++ LibraryApp/library_core/models.py
@@ -1,70 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
-class members(models.Model):
-    member_id=models.CharField(max_length=10, primary_key=True)
-    member_name=models.CharField(max_length=100)
-    member_address=models.CharField(max_length=100)
-    reg_date = models.DateField()
-    
+# Öğrenciye ait ekstra bilgiler
+class StudentExtra(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    enrollment = models.CharField(max_length=40)
+    branch = models.CharField(max_length=40)
+
     def __str__(self):
-     return self.member_name
+        return self.user.first_name + ' [' + self.enrollment + ']'
 
+    @property
+    def get_name(self):
+        return self.user.first_name
 
-class books(models.Model):
-    isbn=models.CharField(max_length=20)
-    book_title=models.CharField(max_length=200)
-    author=models.CharField(max_length=200)
-    category=models.CharField(max_length=100)
-    rental_price = models.DecimalField(max_digits=5, decimal_places=2)
-    status = models.BooleanField(default=True)
-    publisher = models.CharField(max_length=200)
-    
-    def __str__(self):
-     return self.book_title
+    @property
+    def getuserid(self):
+        return self.user.id
 
-class branch(models.Model):
-    branch_id=models.CharField(max_length=10, primary_key=True)
-    manager_id=models.CharField(max_length=10)
-    branch_address=models.CharField(max_length=100)
-    contact_no=models.CharField(max_length=15)
-    
-    def __str__(self):
-      return self.branch_id
-
-
-class employees(models.Model):
-    ROLE_CHOICES = [
-        ('SUPER_ADMIN', 'Super Admin'),
-        ('ADMIN', 'Admin'),
+# Kitap modeli
+class Book(models.Model):
+    catchoice = [
+        ('education', 'Education'),
+        ('entertainment', 'Entertainment'),
+        ('comics', 'Comics'),
+        ('biography', 'Biography'),
+        ('history', 'History'),
     ]
-
-    emp_id = models.CharField(max_length=10, primary_key=True)
-    emp_name = models.CharField(max_length=100)
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
-    branch = models.ForeignKey(branch, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='ADMIN')  # Varsayılan olarak 'ADMIN'
+    name = models.CharField(max_length=50)
+    isbn = models.BigIntegerField()
+    author = models.CharField(max_length=50)
+    category = models.CharField(max_length=30, choices=catchoice, default='education')
 
     def __str__(self):
-        return self.emp_name
+        return f"{self.name} [{self.isbn}]"
 
-class issued_status(models.Model):
-    issued_member = models.ForeignKey(members, on_delete=models.CASCADE)
-    issued_book = models.ForeignKey(books, on_delete=models.CASCADE)
-    issued_emp = models.ForeignKey(employees, on_delete=models.CASCADE)
+# Kitap verildiğinde kaç gün sonra iade edilecek?
+def get_expiry():
+    return datetime.today() + timedelta(days=30)
+
+# Ödünç alınan kitaplar
+class IssuedBook(models.Model):
+    enrollment = models.CharField(max_length=30)
+    isbn = models.CharField(max_length=30)
+    issuedate = models.DateField(auto_now=True)
+    expirydate = models.DateField(default=get_expiry)
     
-    issued_id = models.CharField(max_length=10, primary_key=True)
-    issued_book_isbn = models.CharField(max_length=20)
-    issued_book_name = models.CharField(max_length=200)
-    issued_date = models.DateField()
+    statuschoice = [
+        ('Issued', 'Issued'),
+        ('Returned', 'Returned'),
+    ]
+    status = models.CharField(max_length=20, choices=statuschoice, default='Issued')
 
     def __str__(self):
-        return self.issued_id
-  
-
-class return_status(models.Model):
-    return_id = models.CharField(max_length=10, primary_key=True)
-    issued = models.ForeignKey(issued_status, on_delete=models.CASCADE)
-    return_date = models.DateField()
-
-    def __str__(self):
-        return self.return_id
+        return self.enrollment
