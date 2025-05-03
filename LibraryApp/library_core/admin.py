22cad26fe64e93f14bbe3051095176c0ff92c8fd
Beyzanur <beyzanurdincer502@gmail.com>
2025-05-03 13:04:28 +0300

    Added custom admin views for Book, StudentExtra, and IssuedBook models. Categories now display correctly.

--- LibraryApp/library_core/admin.py
+++ LibraryApp/library_core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import StudentExtra, Book, IssuedBook
 
-# Register your models here.
+# StudentExtra 
+@admin.register(StudentExtra)
+class StudentExtraAdmin(admin.ModelAdmin):
+    list_display = ('user', 'enrollment', 'branch')
+    search_fields = ('user__first_name', 'enrollment', 'branch')
+
+# Book 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'isbn', 'author', 'category_display')
+    search_fields = ('name', 'isbn', 'author')
+    list_filter = ('category',)
+
+    def category_display(self, obj):
+        return obj.get_category_display()
+    category_display.short_description = 'Category'
+
+# IssuedBook 
+@admin.register(IssuedBook)
+class IssuedBookAdmin(admin.ModelAdmin):
+    list_display = ('enrollment', 'isbn', 'issuedate', 'expirydate', 'status')
+    list_filter = ('status', 'issuedate')
+    search_fields = ('enrollment', 'isbn')
+
