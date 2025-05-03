b19e8086211dc1538577ec4867683b045c18ee32
Beyzanur <beyzanurdincer502@gmail.com>
2025-05-03 17:57:05 +0300

    Created forms to collect data from the database and connect with views for student registration and book issuing.

---
+++ LibraryApp/library_core/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Book,StudentExtra,IssuedBook
+from django.contrib.auth.models import User
+
+from LibraryApp.library_core import models
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model=Book
+        fields=['name','isbn','author','category']
+        
+class StudentUserForm(forms.ModelForm):
+    class Meta:
+        model=User 
+        fields=['first_name','last_name','username','password']
+
+
+class StdentExtra(forms.ModelForm):
+    class Meta:
+        model=StudentExtra
+        fields=['enrollment','branch']
+
+class IssuedBook(forms.ModelForm):
+    class Meta:
+        model=IssuedBook
+        fields='_all_'
+class IssuedBookForm(forms.Form):
+    #to_field_name value will be stored when form is submitted.....__str__ method of book model will be shown there in html
+    isbn2=forms.ModelChoiceField(queryset=models.Book.objects.all(),empty_label="Name and isbn", to_field_name="isbn",label='Name and Isbn')
+    enrollment2=forms.ModelChoiceField(queryset=models.StudentExtra.objects.all(),empty_label="Name and enrollment",to_field_name='enrollment',label='Name and enrollment')
+    
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 
-=======
 # LibraryApp
 A modern library automation system with admin features, user management, and book tracking.
 
