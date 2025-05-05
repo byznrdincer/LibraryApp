490527cf2bb0817b2a600b70bf7ddad74b780ff8
Beyzanur <beyzanurdincer502@gmail.com>
2025-05-05 15:24:02 +0300

    "Added URL routes and created skeleton view functions for future implementation."

--- LibraryApp/library_core/views.py
+++ LibraryApp/library_core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_view(request):
+    pass
+
+
+
+
+
+
+
+def adminclick_view(request):
+    pass
+def studentclick_view(request):
+    pass
+def studentsignup_view(request):
+    pass
+def returnbook(request):
+    pass
+def afterlogin_view(request):
+    pass
+def addbook_view(request):
+    pass
+def viewbook_view(request):
+    pass
+def issuebook_view(request):
+    pass
+def viewissuedbook_view(request):
+    pass
+def viewsStudent_view(request):
+    pass
+def viewissuedbookbystudent(request):
+    pass
