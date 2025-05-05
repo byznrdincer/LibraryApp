981d661ea186efe0f5f651baa39b173292bff6c0
Beyzanur <beyzanurdincer502@gmail.com>
2025-05-05 17:36:55 +0300

    "Created view functions for homepage, student and admin login, and student registration."

--- LibraryApp/library_core/views.py
+++ LibraryApp/library_core/views.py
@@ -1,20 +1,63 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from . import forms, models
+from django.contrib.auth.models import Group
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required, user_passes_test
+from datetime import datetime, timedelta, date
+from django.core.mail import send_mail
 
 def home_view(request):
-    pass
-
-
-
-
-
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request, "library_core/index.html")
 
 
 def adminclick_view(request):
-    pass
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request, "library_core/adminclick.html")
+
+
 def studentclick_view(request):
-    pass
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request, "library_core/studentclick.html")
+
+
 def studentsignup_view(request):
-    pass
+    form1 = forms.StudentUserForm()
+    form2 = forms.StudentExtraForm()
+    mydict = {'form1': form1, 'form2': form2}
+    
+    if request.method == 'POST':
+        form1 = forms.StudentUserForm(request.POST)
+        form2 = forms.StudentExtraForm(request.POST)
+        
+        if form1.is_valid() and form2.is_valid():
+            user = form1.save()
+            user.set_password(user.password)
+            user.save()
+            f2 = form2.save(commit=False)
+            f2.user = user
+            user2 = f2.save()
+            
+            my_student_group = Group.objects.get_or_create(name='STUDENT')
+            my_student_group[0].user_set.add(user)
+
+        return HttpResponseRedirect('studentlogin')
+    
+    return render(request, 'library_core/studentsignup.html', context=mydict)
+
+
+
+
+
+
+
+
+
 def returnbook(request):
     pass
 def afterlogin_view(request):
