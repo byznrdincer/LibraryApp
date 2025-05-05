653ea949cf30ded37e4a70af408aa5e9bfc09b1d
Beyzanur <beyzanurdincer502@gmail.com>
2025-05-05 11:12:34 +0300

    URLs for the Django project have been set up.

--- LibraryApp/LibraryApp/urls.py
+++ LibraryApp/LibraryApp/urls.py
@@ -14,8 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+
+from LibraryApp.library_core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('',views.home_view),
+    
+    path('adminclick',views.adminclick_view),
+    path('studentclick',views.studentclick_view),
+    
+    path('studentsignup',views.studentsignup_view),
+    path('adminlogin',LoginView.as_view(template_name='library/adminlogin.html')),
+    path('studentlogin',LoginView.as_view(template_name='library/studentlogin.html')),
+    path('returnbook/<int:id>/',views.returnbook,name='returnbook'),
+    path('logout',LogoutView.as_view(template_name='library/index.html')),
+    path('afterlogin',views.afterlogin_view),
+    path('addbook',views.addbook_view),
+    path('viewbook',views.viewbook_view),
+    path('issuebook',views.issuebook_view),
+    path('viewissuedbook',views.viewissuedbook_view),
+    path('viewstudent',views.viewstudent_view),
+    path('viewissuedbookbystudent',views.viewissuedbookbystudent,name='viewissuedbookbystudent'),
+    
+    
+    
 ]
